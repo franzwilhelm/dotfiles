13e11826fa39aa3191bc7e2e782e928b35a6fe05
franzwilhelm <franz.vonderlippe@gmail.com>
2017-08-30 14:30:00 +0200

    [ADD] DayJournal reminder

--- i3/scripts/date.sh
+++ i3/scripts/date.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+add=""
 if [ $BLOCK_BUTTON == 1 ]; then 
 	com.github.thejambi.dayjournal
 fi
-echo `date '+%Y-%d-%m %H:%M'`
+
+hour=`date '+%H'`
+if (( $hour >= 22 )); then
+	add=" Husk å skrive i dagboka! | "
+fi	
+echo $add `date '+%d.%m.%Y %H:%M'`
